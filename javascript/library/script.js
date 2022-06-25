5d25b739f9142ff4694d4b285cc107203784eea6
Andrew Capro <andrewcapro@gmail.com>
2022-06-25 17:02:21 -0400

    started library

---
+++ javascript/library/script.js
@@ -0,0 +1,16 @@
+let myLibrary = [];
+
+class Book {
+  // the constructor...
+  constructor(title, author, pages, read){
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+  }
+}
+
+function addBookToLibrary() {
+  // do stuff here
+    new constructor(title, author, pages, read)
+}
