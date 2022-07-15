186d2abe7829201bfffbce557dfd863fa4523b93
Andrew Capro <andrewcapro@gmail.com>
2022-07-15 12:01:44 -0400

    started cv maker

--- cv-project/src/App.js
+++ cv-project/src/App.js
@@ -1,10 +1,18 @@
-import './App.css';
+import React,{Component} from 'react';
+import Header from "./components/Header.js"
+import Main from "./components/Main.js"
+import Footer from "./components/Footer.js"
+import "./App.css"
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+export default class App extends Component{
+
+  render() {
+    return (
+      <div className="body">
+        <Header />
+        <Main />
+        <Footer />
+      </div>
+    );
+  }
 }
-
-export default App;
